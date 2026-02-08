3760bc845528775c8863a478895366980851c748
evenmoor <evenmoor4free@gmail.com>
2026-02-08 09:50:26 -0700

    adds initial quest dialog

--- scripts/world.gd
+++ scripts/world.gd
@@ -7,6 +7,10 @@ extends Node2D
 
 @onready var player:CharacterBody2D = $player
 
+#dialogic assets
+@onready var dl_character_player = load("res://addons/dialogic/assets/characters/player.dch")
+@onready var dl_character_villager = load("res://addons/dialogic/assets/characters/villager1.dch")
+
 func updateUI() -> void:
 	#load values from the global PlayerState script
 	player_health_bar.max_value = PlayerState.max_health
@@ -29,3 +33,17 @@ func _ready() -> void:
 	
 #func _process(_delta: float) -> void:
 	#updateUI()#I don't think this is needed.... unless we do some more health testing
+
+
+func _on_villager_1_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		#print("Villager: Hello!")
+		if Dialogic.current_timeline == null: #checks to see if a timeline is running
+			var layout = Dialogic.start("villager1") #starts the labeled timeline
+			layout.register_character(dl_character_player, $player) #allows tracking of the player sprite for the speech bubble
+			layout.register_character(dl_character_villager, $villager1/villagerSprite) 
+
+
+func _on_villager_1_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("Villager: Goodbye!")
